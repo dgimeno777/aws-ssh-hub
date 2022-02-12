5f30496d9c053bbc278f302b5636795423ccd16c
Danny <dgimeno777@gmail.com>
2022-02-12 15:20:19 -0500

    add remote state

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,23 @@
+locals {
+  vpc_id            = data.terraform_remote_state.terraform-aws-vpc.outputs.vpc_id
+  public_subnet_id  = data.terraform_remote_state.terraform-aws-vpc.outputs.subnet_public_a_id
+  private_subnet_id = data.terraform_remote_state.terraform-aws-vpc.outputs.subnet_private_a_id
+}
+
 variable "aws_region" {
-  type = string
+  type        = string
   description = "AWS Region"
-  default = "us-east-1"
+  default     = "us-east-1"
 }
 
 variable "aws_profile" {
-  type = string
+  type        = string
   description = "AWS CLI Profile"
-  default = "aws_ssh_hub"
+  default     = "aws_ssh_hub"
+}
+
+variable "terraform_aws_vpc_workspace" {
+  type        = string
+  description = "Workspace of the terraform-aws-vpc remote state"
+  default     = "default"
 }
